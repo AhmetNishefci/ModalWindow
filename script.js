4383fdd78541f77ac6414c98c9bf6f9fc0919e4e
AhmetNishefci <metti.ni@hotmail.com>
2022-04-20 01:37:03 +0200

    Select the elements we will work with

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const showModal = document.querySelectorAll(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+
+console.log(closeModal);
+
+for (let i = 0; i < showModal.length; i++) {
+  console.log(showModal[i].textContent);
+}
