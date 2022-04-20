32facb36d568c55f327cad823508af6d0c3b017d
AhmetNishefci <metti.ni@hotmail.com>
2022-04-20 02:01:59 +0200

    Working with classes

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const overlay = document.querySelector(".overlay");
 const showModal = document.querySelectorAll(".show-modal");
 const closeModal = document.querySelector(".close-modal");
 
-console.log(closeModal);
+const hide = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const show = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 for (let i = 0; i < showModal.length; i++) {
-  console.log(showModal[i].textContent);
+  showModal[i].addEventListener("click", show);
 }
+
+closeModal.addEventListener("click", hide);
+
+overlay.addEventListener("click", hide);
